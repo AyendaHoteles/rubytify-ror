904e8f69f5e0220c4b6ad97f5f60613693b298a7
Felix Torrenegra G <f.torrenegrag@gmail.com>
2019-06-23 07:53:21 -0500

    fix migration

--- db/migrate/20190623112824_change_total_tracks_to_be_integer.rb
+++ db/migrate/20190623112824_change_total_tracks_to_be_integer.rb
@@ -1,5 +1,9 @@
 class ChangeTotalTracksToBeInteger < ActiveRecord::Migration[5.2]
   def change
-    change_column :albums, :total_tracks, :integer, default: 0
+    if Rails.env.production?
+      change_column :albums, :total_tracks, 'integer USING CAST(total_tracks AS integer)', default: 0
+    else
+      change_column :albums, :total_tracks, :integer, default: 0
+    end
   end
 end
