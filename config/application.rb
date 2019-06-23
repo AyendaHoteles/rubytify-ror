e92f3b1a31cc5fc2bdbea65a4ef870d2e2015c0c
Felix Torrenegra G <f.torrenegrag@gmail.com>
2019-06-23 07:44:46 -0500

    add CORS config

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,12 @@ module AyendaTest
     config.autoload_paths += %W(#{config.root}/services)
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i(get post put patch delete options head)
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
