9f192e5c6c55ee5704f19eef76a0db6ecd869f7a
Felix Torrenegra G <f.torrenegrag@gmail.com>
2019-06-23 07:30:40 -0500

    complete functions in controllers

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::AlbumsController < ApplicationController
   def index
+    artist_id = params[:artist_id]
+    artist = Artist.preload(:albums).find_by_id(artist_id)
+    return render json: {data: []}, status: :not_found unless artist
+    render json: {data: artist.albums}, status: :ok
   end
 end
--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::SongsController < ApplicationController
   def index
+    album_id = params[:album_id]
+    album = Album.preload(:songs).find_by_id(album_id)
+    return render json: {data: []}, status: :not_found unless album
+    render json: {data: album.songs}, status: :ok
   end
   def random
+    songs = Song.joins(album: :artist).where("artists.genres like ?", "%#{params[:genre_name]}%").uniq
+    render json: {data: [songs.sample]}, status: :ok
   end
 end
