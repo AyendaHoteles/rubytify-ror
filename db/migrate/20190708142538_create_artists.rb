cdb49b295b8e79790129348852a32ebf74943332
Juan Camilo Hernández <camilo9020@gmail.com>
2019-07-08 09:31:57 -0500

    create artists model

---
+++ db/migrate/20190708142538_create_artists.rb
@@ -0,0 +1,13 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.text :genres
+      t.string :popularity
+      t.text :spotify_url
+      t.text :spotify_id
+
+      t.timestamps
+    end
+  end
+end
