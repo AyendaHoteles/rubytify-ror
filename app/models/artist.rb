cdef8757540d330f0e4657b3267c1b2dd5e65fe1
Juan Camilo Hernández <camilo9020@gmail.com>
2019-07-08 18:39:08 -0500

    Add scope to get random artist by genre

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,5 @@ class Artist < ApplicationRecord
   has_many :albums
   has_many :songs
   serialize :image
+  scope :get_artists_by_genre, -> (genre_name) { where(" genres LIKE '%#{genre_name}%'") }
 end
