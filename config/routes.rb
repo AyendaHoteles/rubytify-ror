a75a4ad58553ee869174ad99486b5099e4e6d5e7
Damian Galindo Meza <damiangalindomeza@gmail.com>
2019-07-09 07:19:42 -0500

    Redirect to apidoc on root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount OkComputer::Engine, at: '/check_health'
   mount API::Base, at: '/api'
   mount GrapeSwaggerRails::Engine, at: '/apidoc'
+
+  root to: redirect('/apidoc')
 end
 
 # == Route Map
@@ -16,11 +18,11 @@ end
 #        rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
 # update_rails_disk_service PUT  /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
 #      rails_direct_uploads POST /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
-# 
+#
 # Routes for OkComputer::Engine:
 #              root GET|OPTIONS /                 ok_computer/ok_computer#show {:check=>"default"}
 # okcomputer_checks GET|OPTIONS /all(.:format)    ok_computer/ok_computer#index
 #  okcomputer_check GET|OPTIONS /:check(.:format) ok_computer/ok_computer#show
-# 
+#
 # Routes for GrapeSwaggerRails::Engine:
 #   root GET  /           grape_swagger_rails/application#index
