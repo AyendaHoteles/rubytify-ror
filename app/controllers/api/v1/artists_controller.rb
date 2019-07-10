441c263b8c01845bc4e9b16446742774f11d7a3b
Juan Camilo Hernández <camilo9020@gmail.com>
2019-07-09 19:10:46 -0500

    order by popularity

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArtistsController < ApplicationController
   def index
-    artists = Artist.all
+    artists = Artist.all.order('popularity DESC')
     if artists
       render json: { data: serializable_resource(artists) }, status: :ok
     else
