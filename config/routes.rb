53e96af3e94410a598aa094a7ad109db151b8bed
James Thorp <jamesthorp1@gmail.com>
2019-08-26 19:00:43 +0100

    routes and file structure for artists controller, also added env to .gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "artists#home"
+  namespace :api do
+    namespace :v1 do
+      resources :artists, only: [:index] do
+        resources :albums do
+          resources :songs
+        end
+      end
+    end
+  end
 end
