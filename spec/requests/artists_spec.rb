c3094fc54671261842af4f236ad89879cebfce07
James Thorp <jamesthorp1@gmail.com>
2019-08-26 21:15:45 +0100

    extra artists controller rspec test

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -18,5 +18,13 @@ RSpec.describe Api::V1::ArtistsController, :type => :controller do
       expect(data.first['id'].to_i).to eq artist_two.id
       expect(data.last['id'].to_i).to eq artist_one.id
     end
+
+    it "returns correct data" do
+      data = JSON.parse(response.body)['data']
+      updated_artist_one = Artist.find(artist_one.id)
+      expect(data.last['id'].to_i).to eq updated_artist_one.id
+      expect(data.last['popularity']).to eq updated_artist_one.popularity
+      expect(data.last['genres']).to eq updated_artist_one.genres
+    end
   end
 end
