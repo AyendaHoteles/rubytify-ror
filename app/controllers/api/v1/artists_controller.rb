f2b1cd7184a69cb03b27b4e013ad313324f53de3
James Thorp <jamesthorp1@gmail.com>
2019-08-26 19:11:15 +0100

    added rspotify gem and working artists controller

---
+++ app/controllers/api/v1/artists_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::ArtistsController < ApplicationController
+  before_action :spotify_authenticate
+
+  def index
+    @artists = Artist.all.order(popularity: :desc)
+    if @artists.where(spotify_id: nil).present?
+      @missing_info = @artists.where(spotify_id: nil).each do |artist|
+        spot_artist = RSpotify::Artist.search(artist.name).first
+        if spot_artist.present?
+          artist.update(
+            spotify_id: spot_artist.id,
+            genres: spot_artist.genres,
+            popularity: spot_artist.popularity,
+            spotify_url: spot_artist.external_urls["spotify"],
+            image: spot_artist.images.first)
+        end
+      end
+    end
+    @artists_json = build_artists_json(@artists)
+
+    render json: @artists_json
+  end
+
+  private
+
+  def spotify_authenticate
+    RSpotify.authenticate(ENV['SPOTIFY_ID'], ENV['SPOTIFY_SECRET'])
+  end
+
+  def build_artists_json(artists)
+    required_json = Hash.new { |h, k| h[k] = [] }
+
+    artists.map do |artist|
+      required_json['data'] << {
+        id: "#{artist.id}", 
+        name: "#{artist.name}", 
+        popularity: "#{artist.popularity}", 
+        genres: artist.genres, 
+        spotify_url: "#{artist.spotify_url}",
+        image: "#{artist.image}" }
+    end
+    required_json
+
+  end
+end
