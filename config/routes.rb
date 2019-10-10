90e511e5af93ba2b6a2b4a650e68b551b182bc78
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 17:41:36 -0500

    api routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :artists, only: %i[index show] do
+        resources :albums, only: %i[index]
+      end
+      resources :albums, only: %i[index show] do
+        resources :songs, only: %i[index]
+      end
+      resources :genres, only: %i[index show] do
+        member do
+          get "random"
+        end
+      end
+    end
+  end
 end
