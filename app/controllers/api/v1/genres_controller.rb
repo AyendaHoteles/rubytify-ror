76581793c3beda646c2f5a8ef4f775706ee7982f
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 19:24:25 -0500

    show and random methods passing correct instance variables to view

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -1,25 +1,36 @@
 class Api::V1::GenresController < Api::V1::BaseController
-  before_action :find_genre, only: %i[show random]
+  before_action :find_genre, only: %i[index show random]
+  before_action :all_genres, only: %i[index show random]
 
   def index
-    @genres = Genre.all
+    @genres.keys
   end
 
   def show
-    @total_tracks = songs_of_array.size
+    find_artists_of_genre
   end
 
   def random
-    @song = songs_of_array.sample
+    @artists = find_artists_of_genre
+    @song = Genre.songs_of(@artists).sample
   end
 
   private
 
   def find_genre
-    @genre = Genre.find_by(name: params[:id].capitalize)
+    @genre = params[:id]
   end
 
   def songs_of_array
     Song.where(genre: @genre)
+  end
+
+  def all_genres
+    @genres = Genre.genres
+  end
+
+  def find_artists_of_genre
+    @artists = []
+    @artists << @genres[@genre]
   end
 end
