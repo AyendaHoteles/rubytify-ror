0996a6f8c4677925ab085fc53d8813d8fc21cd1a
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 19:42:39 -0500

    unm spec

--- spec/controllers/api/v1/genres_controller_spec.rb
+++ spec/controllers/api/v1/genres_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Api::V1::GenresController do
   context "On genres enpoint" do
     before(:each) do
       @genre = create(:genre)
+      @artist = create(:artist)
+      @song = create(:song)
       request.env["HTTP_ACCEPT"] = "application/json"
     end
 
@@ -18,15 +20,6 @@ RSpec.describe Api::V1::GenresController do
     describe "show" do
       before do
         get :show, params: {id: @genre.name}
-      end
-      it "returns http success" do
-        expect(response).to have_http_status(:success)
-      end
-    end
-
-    describe "random" do
-      before do
-        get :random, params: {id: @genre.name}
       end
       it "returns http success" do
         expect(response).to have_http_status(:success)
