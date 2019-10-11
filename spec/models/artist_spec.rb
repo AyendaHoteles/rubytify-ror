ec36130ea6725315149aa79f3969667fbb1ecc28
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 20:02:29 -0500

    artist model tests

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,6 +1,24 @@
 require "rails_helper"
 
-# RSpec.describe Artist, type: :model do
-#   context "validation tests" do
-#     it "ensures "
-# end
+RSpec.describe Artist, :type => :model do
+  context "validation tests" do
+    before(:each) do
+      @artist = build(:artist)
+    end
+    it "is valid with valid attributes" do
+      expect(@artist).to be_valid
+    end
+    it "is not valid without a name" do
+      @artist.name = ""
+      expect(@artist).to_not be_valid
+    end
+    it "is not valid without a spotify_url" do
+      @artist.spotify_url = ""
+      expect(@artist).to_not be_valid
+    end
+    it "is not valid without a spotify_id" do
+      @artist.spotify_id = ""
+      expect(@artist).to_not be_valid
+    end
+  end
+end
