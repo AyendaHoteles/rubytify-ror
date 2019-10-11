3e55ed263ec6aed15316c824e293b5afbaa20b1f
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 21:01:55 -0500

    album model specs

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,30 @@
 require "rails_helper"
 
-# RSpec.describe Album, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Album, :type => :model do
+  context "validation tests" do
+    before(:each) do
+      @album = build(:album)
+    end
+    describe "validations" do
+      it "is valid with valid attributes" do
+        expect(@album).to be_valid
+      end
+      it "is not valid without a name" do
+        @album.name = ""
+        expect(@album).to_not be_valid
+      end
+      it "is not valid without an spotify_url" do
+        @album.spotify_url = ""
+        expect(@album).to_not be_valid
+      end
+      it "is not valid without an spotify_id" do
+        @album.spotify_id = ""
+        expect(@album).to_not be_valid
+      end
+    end
+    describe "associations" do
+      it { should belong_to(:artist) }
+      it{ is_expected.to have_many(:songs) }
+    end
+  end
+end
