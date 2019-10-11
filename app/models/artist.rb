b7dd956b50cee5a6cf55b7e4e441ffb685621b7a
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 20:02:48 -0500

    validates presence on artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,5 @@ class Artist < ApplicationRecord
   has_many :albums
   has_many :songs, through: :albums
   serialize :genres, Array
+  validates :name, :spotify_url, :spotify_id, presence: true
 end
