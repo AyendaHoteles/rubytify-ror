31abdce5ac1c80c1b4daf6077c594555d18d4108
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 21:02:24 -0500

    song model specs

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,33 @@
 require "rails_helper"
 
-# RSpec.describe Song, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Song, :type => :model do
+  context "validation tests" do
+    before(:each) do
+      @song = build(:song)
+    end
+    describe "validations" do
+      it "is valid with valid attributes" do
+        expect(@song).to be_valid
+      end
+      it "is not valid without a name" do
+        @song.name = ""
+        expect(@song).to_not be_valid
+      end
+      it "is not valid without an spotify_url" do
+        @song.spotify_url = ""
+        expect(@song).to_not be_valid
+      end
+      it "is not valid without an spotify_id" do
+        @song.spotify_id = ""
+        expect(@song).to_not be_valid
+      end
+      it "is not valid without an duration_ms" do
+        @song.duration_ms = ""
+        expect(@song).to_not be_valid
+      end
+    end
+    describe "associations" do
+      it { should belong_to(:album) }
+    end
+  end
+end
