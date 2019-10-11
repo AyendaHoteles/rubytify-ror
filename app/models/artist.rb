8bb1d245bbea168f5ea1fc248ae139d87772dfc1
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 23:13:49 -0500

    dependencies destroy on models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :albums
+  has_many :albums, dependent: :destroy
   has_many :songs, through: :albums
   serialize :genres, Array
   validates :name, :spotify_url, :spotify_id, presence: true
