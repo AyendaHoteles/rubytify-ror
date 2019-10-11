0b27531831c4b0fc33b8f60291b67c861b63ce2b
Daniel Lopez <dlopez.10g@gmail.com>
2019-10-10 19:39:02 -0500

    adjust on rspec it params

--- spec/controllers/api/v1/genres_controller_spec.rb
+++ spec/controllers/api/v1/genres_controller_spec.rb
@@ -17,19 +17,16 @@ RSpec.describe Api::V1::GenresController do
     end
     describe "show" do
       before do
-        get :show, params: {id: @genre.name.capitalize}
+        get :show, params: {id: @genre.name}
       end
       it "returns http success" do
         expect(response).to have_http_status(:success)
-      end
-      it "returns data array" do
-        expect(response.body).to include("data")
       end
     end
 
     describe "random" do
       before do
-        get :random, params: {id: @genre.id}
+        get :random, params: {id: @genre.name}
       end
       it "returns http success" do
         expect(response).to have_http_status(:success)
