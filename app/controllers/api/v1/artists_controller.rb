fc27447bc304575bf97eae156bb46e2b4aeb182e
david <davidndy@gmail.com>
2019-10-12 19:10:58 -0500

    add dummy response in controllers

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ArtistsController < ApplicationController
+  def index
+     render json: {name: "david"}.to_json , status: 201
+  end
+
+  def albums
+    render json: {name: "metallica"}.to_json , status: 201
+  end
 end
--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::AlbumsController < ApplicationController
+class Api::V1::SongsController < ApplicationController
   def index
     render json: {name: "metallica"}.to_json , status: 201
   end
