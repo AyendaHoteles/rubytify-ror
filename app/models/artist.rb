cac59aa7734a1d97605f6b600a9bc26147721db0
andres-martin <andresmartin266@gmail.com>
2019-10-19 15:57:08 -0500

    [add] annotate models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  image       :text
+#  popularity  :integer
+#  spotify_url :text
+#  spotify_id  :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Artist < ApplicationRecord
 end
--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  image       :text
+#  popularity  :integer
+#  spotify_url :text
+#  spotify_id  :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
