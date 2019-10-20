7040467efaaf06bd0bdcd9bc9f22fdd9bf0a1706
andres-martin <andresmartin266@gmail.com>
2019-10-19 23:06:52 -0500

    [feature] add a new route api/v1/artists/:id/albums

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,12 @@
 class ArtistsController < ApplicationController
     def index
         @artists = Artist.all.order('popularity DESC')
-        render json: { data: @artists.as_json(except: [:spotify_id, :created_at, :updated_at]) }
+        render json: { data: @artists.as_json(except: [:spotify_id, :created_at, :updated_at]) }, status: :ok
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+        render json: { data: @artist.albums.as_json(except: 
+                        [:created_at, :updated_at, :spotify_id, :artist_id]) }, status: :ok
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope :api, defaults: {format: :json} do
     get 'v1/artists', to: 'artists#index'
+    get 'v1/artists/:id/albums', to: 'artists#show'
+    # resources :artists
   end
 end
