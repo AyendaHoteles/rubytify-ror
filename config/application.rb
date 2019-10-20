1c95573cce63bf8cc0e79c1a98866412d743e991
andres-martin <andresmartin266@gmail.com>
2019-10-20 15:28:09 -0500

    [fix] CORS tested and enabled

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,13 @@ module AyendaTest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     # CORS enabled to all incoming origins
-
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
