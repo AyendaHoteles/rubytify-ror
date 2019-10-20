00f522f1f07652087925a7ed4985c2047e8ff4bb
andres-martin <andresmartin266@gmail.com>
2019-10-20 16:46:55 -0500

    [add] specs for Album model validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -15,6 +15,6 @@
 
 class Album < ApplicationRecord
   belongs_to :artist
-  has_many :songs
-  validates_presence_of :name, :image, :spotify_url, :total_tracks, :spotify_id, :artist_id
+  has_many :songs, dependent: :destroy
+  validates_presence_of :name, :image, :spotify_url, :total_tracks, :spotify_id, :artist_id, :created_at
 end
