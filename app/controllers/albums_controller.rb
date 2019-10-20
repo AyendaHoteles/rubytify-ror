5f35e7b0f21dc9a64741c65ab703e4ca67509058
andres-martin <andresmartin266@gmail.com>
2019-10-20 12:44:23 -0500

    [add] genres route to get random song working exceptions handling implemented in all controllers

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,11 @@
 class AlbumsController < ApplicationController
-    def show
-        @album = Album.find(params[:id])
-        render json: { data: @album.songs.as_json(except: 
-                        [:id, :created_at, :updated_at, :album_id, :spotify_id]) }, status: :ok
+    def songs
+        begin
+            album = Album.find(params[:id])
+            render json: { data: album.songs.as_json(except: 
+                [:id, :created_at, :updated_at, :album_id, :spotify_id]) }, status: :ok
+        rescue => exception
+            render json: {error: "album does not exist"}, status: 404 
+        end
     end
 end
