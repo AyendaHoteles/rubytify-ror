028a406792ee8082ea21a954c566407478e3b64f
Natalia <nahrivera7@gmail.com>
2019-10-20 16:49:40 -0500

    Change sqlite3 for pg to be able to deploy into Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_19_173355) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.string "image"
