a10b52f6bea250b4fcab491aa2b82c452c2b2ce8
andres-martin <andresmartin266@gmail.com>
2019-10-20 19:10:46 -0500

    [add] configure root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'artists#index'
+
   scope :api, defaults: {format: :json} do
     get 'v1/artists', to: 'artists#index'
     get 'v1/artists/:id/albums', to: 'artists#albums'
