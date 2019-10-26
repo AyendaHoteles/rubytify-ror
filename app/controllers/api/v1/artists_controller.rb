d73ea6f431d30de0037c04a07987d6a4c6ad8ab7
josevasquez <jous32@gmailcom>
2019-10-26 18:02:18 -0500

    sort by popularity

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArtistsController < ApplicationController
   def index
-    render json: {data: Artist.all}, status: :ok
+    render json: {data: Artist.all.order(popularity: :desc)}, status: :ok
   end
 end
