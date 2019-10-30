628cc0d479d32fcd25c23851faa01f66f8d07584
Daniel Morales <jdanielmo@gmail.com>
2019-10-29 19:13:13 -0500

    fixes bug in migration file

--- db/migrate/20191029154229_add_genres_to_artist.rb
+++ db/migrate/20191029154229_add_genres_to_artist.rb
@@ -1,5 +1,5 @@
 class AddGenresToArtist < ActiveRecord::Migration[5.2]
   def change
-    add_column :artists, :genres, :string, array: true, default: [].to_yaml
+    add_column :artists, :genres, :string, array: true, default: []
   end
 end
