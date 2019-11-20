a87b9dba6563ad36477de99bb13ed58bdce7b628
Jhon Arias <jhonarmont@hotmail.com>
2019-11-20 02:29:45 -0500

    change albums to a regular route.
    This is because we don't need any resource for album itself

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
       resources :artists, only: [:index] do
         get '/:id/albums', to:"artists#albums", on: :collection, as: 'artist_albums'
       end
-      resources :albums, except: [:index, :create, :show, :update, :destroy] do
-        get '/:id/songs', to: "albums#songs", on: :collection, as: :album_songs
-      end
+      get 'albums/:id/songs', to: "albums#songs", as: 'album_songs'
       get 'genres/:genre_name/random_song', to: 'songs#random_song', as: 'random_song'
     end
   end
