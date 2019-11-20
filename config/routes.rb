1e0de4737b32d2b6e70095af68c2b0f523fd8a19
Jhon Arias <jhonarmont@hotmail.com>
2019-11-20 02:02:12 -0500

    move controllers into api/v1 folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :songs
-  resources :albums
-  resources :artists
+  namespace 'api' do
+    namespace 'v1' do
+      resources :artists, only: [:index] do
+        get '/:id/albums', to:"artists#albums", on: :collection, as: 'artist_albums'
+      end
+      resources :albums, except: [:index, :create, :show, :update, :destroy] do
+        get '/:id/songs', to: "albums#songs", on: :collection, as: :album_songs
+      end
+      get 'genres/:genre_name/random_song', to: 'songs#random_song', as: 'random_song'
+    end
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
