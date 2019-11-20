a9cd2a57501ce26edbdb9f6359ffe86ebc44fff6
LeelaPte <sebastianzapatacolorado@gmail.com>
2019-11-20 00:01:06 -0500

    models, controllers and routes creation

---
+++ app/controllers/api/v1/songs_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::SongsController < ApplicationController
+  def index
+    raise params.inspect
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      scope 'albums/:id' do
+        resources :songs, defaults: { format: :json }, only: [:index]
+      end
+      scope 'artists/:id' do
+        resources :albums, defaults: { format: :json }, only: [:index]
+      end
+      resources :artists, defaults: { format: :json }, only: [:index]
+    end
+  end
 end
