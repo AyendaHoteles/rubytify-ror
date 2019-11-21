664bd8b65e96525d16904a76054fdffd670230b0
Jhon Arias <jhonarmont@hotmail.com>
2019-11-21 02:58:25 -0500

    add tests for models. Validates fields

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe "Test validations" do
     it "validate presence of fields" do
       should validate_presence_of(:name)
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Test validations" do
+    it "validate presence of model fields" do
+      should validate_presence_of(:name)
+      should validate_presence_of(:preview_url)
+      should validate_presence_of(:duration_ms)
+
+      # Warning from shoulda-matchers:
+      # You are using `validate_inclusion_of` to assert booleans. Be aware that it
+      # ... Hence, you should consider removing this test.
+      # should validate_inclusion_of(:explicit).
+      #   in_array([true, false])
+      should validate_presence_of(:spotify_url)
+      should validate_presence_of(:spotify_id)
+    end
+  end
 end
