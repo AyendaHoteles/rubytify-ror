8c14bc0e521cd36981517d1bf57f17db5b11a667
Jhon Arias <jhonarmont@hotmail.com>
2019-11-21 03:33:59 -0500

    Add action to be called

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ArtistsController < ApplicationController
   def index
     @artists = Artist.order(popularity: :desc)
-    render json: {data: @artists}, except: [:spotify_id, :updated_at, :created_at]
+    render json: { data: @artists }, except: [:spotify_id, :updated_at, :created_at]
+  end
+
+  def albums
   end
 end
