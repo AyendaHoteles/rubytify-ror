ce2e1acfcbf4907b64f47cbe8d818affbf85d62d
Jhon Arias <jhonarmont@hotmail.com>
2019-11-21 14:10:39 -0500

    remove validation of preview_url as it can be nil

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
-  validates :name, :spotify_url, :preview_url, :duration_ms, :spotify_url, :spotify_id, presence: true
+  validates :name, :spotify_url, :duration_ms, :spotify_url, :spotify_id, presence: true
   validates :explicit, inclusion: { in: [true, false] }
   belongs_to :album
 end
