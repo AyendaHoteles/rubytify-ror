908849b0f20342bf2446ca1879491f1f4cbeb21e
Jhon Arias <jhonarmont@hotmail.com>
2019-11-20 20:16:11 -0500

    Add validations to test for every required field

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -4,8 +4,17 @@ RSpec.describe Artist, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   describe "Test validations" do
-    it "validate presence of name" do
+    it "validate presence of fields" do
       should validate_presence_of(:name)
+      should validate_presence_of(:image)
+      should validate_presence_of(:genres)
+      should validate_presence_of(:popularity)
+      should validate_presence_of(:spotify_url)
+      should validate_presence_of(:spotify_id)
+    end
+    
+    it "validate has_many albums" do
+      should have_many(:albums)
     end
   end
 end
