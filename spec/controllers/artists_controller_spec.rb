3cfe3876041e36254cb62551eb7be49a263c0df5
Jhon Arias <jhonarmont@hotmail.com>
2019-11-21 03:29:51 -0500

    Add test for routes

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -1,8 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Api::V1::ArtistsController, type: :controller do
-  describe "test correct route and method" do
-    it "test get /artists" do
+  describe "test correct routes and methods" do
+    it "test get /api/v1/artists route" do
+      should route(:get, "/api/v1/artists").to(action: :index)
+    end
+
+    it "test get /api/v1/artists/:id/albums route" do
+      should route(:get, "/api/v1/artists").to(action: :albums)
     end
   end
 end
