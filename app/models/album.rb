7058cde0c9904aafec126bf835a3c771b0a538dd
Jhon Arias <jhonarmont@hotmail.com>
2019-11-21 02:58:46 -0500

    add validations for each model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :artist
+  validates :name, :image, :spotify_url, :total_tracks, :spotify_id, presence: true
   has_many :songs
-
+  validates_associated :songs
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ApplicationRecord
     validates :name, :image, :genres, :popularity, :spotify_url, :spotify_id, presence: true
     has_many :albums
+    validates_associated :albums
 end
