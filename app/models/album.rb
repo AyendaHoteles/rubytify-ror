2ab0387d59d51b1d1a0663a4492af38b50dd772b
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 16:25:39 -0500

    add scope to get albums for all artists_ids

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,6 @@
 class Album < ApplicationRecord
+  scope :artists_filter, ->(artists_ids) { where("artist_id IN (?)", artists_ids) }
+
   belongs_to :artist
   validates :name, :image, :spotify_url, :total_tracks, :spotify_id, presence: true
   has_many :songs
