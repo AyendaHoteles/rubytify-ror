24728c3c0d3bf9fce4d83f6fa6fa4f56efdc748f
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 16:24:39 -0500

    Create albums action. Render all albums for artist

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::ArtistsController < ApplicationController
+  # get all artists
   def index
     @artists = Artist.order(popularity: :desc)
     render json: { data: @artists }, except: [:spotify_id, :updated_at, :created_at]
   end
 
+  # get all albums for an artist
   def albums
+    @artist = Artist.find(params[:id])
+    @albums = Album.where(artist_id: @artist.id)
+    render json: { data: @albums }, except: [:spotify_id, :updated_at, :created_at, :artist_id]
   end
 end
