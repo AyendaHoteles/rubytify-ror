16074cbb92e4f883d6ea723f29d2fbcdb678a3bd
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 16:24:17 -0500

    create songs action. render all songs for album

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::AlbumsController < ApplicationController
+  # get all songs for an album
+  def songs
+    @album = Album.find(params[:id])
+    @songs = Song.where(album_id: @album.id)
+    render json: { data: @songs }, except: [:id, :spotify_id, :updated_at, :created_at, :album_id]
+  end
 end
