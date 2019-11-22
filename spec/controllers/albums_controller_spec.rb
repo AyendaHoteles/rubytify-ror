b907831a6914108161e2f0290cd1c7e39a130c07
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 16:36:14 -0500

    Add tests for routes of album and song controllers

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Api::V1::AlbumsController, type: :controller do
-
+  describe "test correct routes and methods" do
+    it "test get /api/v1/albums/:id/songs route" do
+      should route(:get, "/api/v1/albums/30/songs").to(action: :songs, id: 30)
+    end
+  end
 end
--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -1,4 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::SongsController, type: :controller do
+  describe "test correct routes and methods" do
+    it "test get /api/v1/genres/:genre_name/random_song route" do
+      should route(:get, "/api/v1/genres/rock/random_song").to(action: :random_song, genre_name: "rock")
+    end
+  end
 end
