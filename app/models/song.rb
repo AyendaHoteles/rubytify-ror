97e86346f4fe294fb4fb814ed77c477bbb7c22af
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 16:26:52 -0500

    add scope to get songs for all albums_ids

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ApplicationRecord
+  scope :album_filter, ->(album_id) { where("album_id IN (?)", album_id) }
+
   validates :name, :spotify_url, :duration_ms, :spotify_url, :spotify_id, presence: true
   validates :explicit, inclusion: { in: [true, false] }
   belongs_to :album
