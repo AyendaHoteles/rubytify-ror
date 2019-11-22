7d76257de081577ea50118d0f617e0408daeb5c0
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 16:26:29 -0500

    Add scope to get artists for a genre

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,11 @@
 class Artist < ApplicationRecord
-    validates :name, :image, :genres, :popularity, :spotify_url, :spotify_id, presence: true
-    has_many :albums
-    validates_associated :albums
+  # scope to get the artist that contains genre_name
+  # on his genres array.
+  # %\"#{genre_name}\"% synstax is used to look for the specific genre_name
+  # and not variatios that contains it as genres is a string and not array
+  scope :genre_filter, ->(genre_name) { where("genres LIKE ?", "%\"#{genre_name}\"%") }
+
+  validates :name, :image, :genres, :popularity, :spotify_url, :spotify_id, presence: true
+  has_many :albums
+  validates_associated :albums
 end
