ca0c48cf704139bf53d66e56944ee97173017661
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 16:36:29 -0500

    remove validatation as  value can be nil

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Song, type: :model do
   describe "Test validations" do
     it "validate presence of model fields" do
       should validate_presence_of(:name)
-      should validate_presence_of(:preview_url)
       should validate_presence_of(:duration_ms)
 
       # Warning from shoulda-matchers:
