fef8e5fd8b108abaaf136ed33cafc8c653b47ac3
Jhon Arias <jhonarmont@hotmail.com>
2019-11-22 19:03:56 -0500

    change db to psql

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  namespace 'api' do
-    namespace 'v1' do
+  root :to => "api/v1/artists#index"
+  namespace "api" do
+    namespace "v1" do
       resources :artists, only: [:index] do
-        get '/:id/albums', to:"artists#albums", on: :collection, as: 'artist_albums'
+        get "/:id/albums", to: "artists#albums", on: :collection, as: "artist_albums"
       end
-      get 'albums/:id/songs', to: "albums#songs", as: 'album_songs'
-      get 'genres/:genre_name/random_song', to: 'songs#random_song', as: 'random_song'
+      get "albums/:id/songs", to: "albums#songs", as: "album_songs"
+      get "genres/:genre_name/random_song", to: "songs#random_song", as: "random_song"
     end
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
