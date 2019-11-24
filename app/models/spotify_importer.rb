76841960a7f3005b0f0f20597f5e643458f276ae
roberto rueda <rueda.roberto89@gmail.com>
2019-11-24 15:53:39 -0500

    Create SpotifyImporter class for rake task

--- app/models/spotify_importer.rb
+++ app/models/spotify_importer.rb
@@ -1,16 +1,57 @@
 class SpotifyImporter
 
-    def initialize
-      
+  attr_reader :artist_list
+
+    def initialize()
+      @artist_list = artist_list
     end
 
-    artists_list["artists"].each do |artist|
-        artist_info = RSpotify::Artist.search(artist.to_s).first
-        if artist_info
-            artist = create_artist(artist_info)
-            create_artist_albums(artist_info.albums, artist)
-        else
-            puts "Artist #{artist} not found"
+    def call
+    artist_list.each do |artist|
+      spotify_artist = RSpotify::Artist.search(artist).first
+      if spotify_artist
+        artist = create_artist(spotify_artist)
+
+        spotify_artist.albums.each do |spotify_album|
+          album = create_album(artist,spotify_album)
+
+          spotify_album.tracks.each do |spotify_track|
+            create_song(album, spotify_track)
+          end
         end
+      else
+        puts "Artist #{artist} not found"
+      end
     end
+
+    def create_artist(spotify_artist)
+      artist = Artist.create(
+        name: spotify_artist.name,
+        image: spotify_artist.images.first["url"]
+        genres: spotify_artist.genres,
+        popularity: spotify_artist.popularity,
+        spotify_url: spotify_artist.href,
+        spotify_id: spotify_artist.id
+      )
+    end
+
+    def create_album(artist, spotify_album)
+      artist.albums.create(
+        name: spotify_album.name,
+        image: spotify_album.images,first["url"],
+        spotify_url: spotify_album.href,
+        total_tracks: spotify_album.total_tracks,
+        spotify_id: spotify_album.id
+      )
+    end
+
+    def create_song(album, spotify_track)
+      album.songs.create(
+        name:spotify_track.name,
+        spotify_url: spotify_track.href,
+        preview_url: spotify_track.preview_url,
+        duration_ms: spotify_track.explicit,
+        spotify_id: spotify_ttack.id
+      )
+    end  
 end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,10 +1,11 @@
 require 'rspotify'
+require 'spotify_importer'
 
 namespace :import do
   desc "Feed DB with artist's info from Spotify"
   task spotify: :environment do
-    artists_list = YAML.load_file(Rails.root.join("db", "seeds", "artists.yml"))
+    artists_list = YAML.load_file(Rails.root.join("lib", "seeds", "artists.yml"))
     
+    SpotifyImporter.new(artist_list["artists"]).call
   end
-
 end
