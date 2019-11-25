6030bdfbc7ffd10253f5ec0cec746fa856393cca
roberto rueda <rueda.roberto89@gmail.com>
2019-11-25 15:31:50 -0500

    Fix SpotifyImporter

--- lib/spotify_importer.rb
+++ lib/spotify_importer.rb
@@ -5,13 +5,13 @@ class SpotifyImporter
   end
   def call
     artist_list.each do |artist|
-      spotify_artist = RSpotify::Artist.search(artist).first
+      spotify_artist = RSpotify::Artist.search(artist.to_s).first
       if spotify_artist
         artist = create_artist(spotify_artist)
         spotify_artist.albums.each do |spotify_album|
           album = create_album(artist, spotify_album)
           spotify_album.tracks.each do |spotify_track|
-            song = create_song(album, spotify_track)
+            create_song(album, spotify_track)
           end
         end
       else
@@ -20,7 +20,7 @@ class SpotifyImporter
     end
   end
   def create_artist(spotify_artist)
-    artist = Artist.find_or_create_by(spotify_id: spotify_artist.id) do |artist|
+    Artist.find_or_create_by(spotify_id: spotify_artist.id) do |artist|
       puts "Creating #{artist.name} in the DB"
       artist.name = spotify_artist.name
       artist.image = spotify_artist.images.first["url"]
@@ -32,7 +32,7 @@ class SpotifyImporter
     end
   end
   def create_album(artist, spotify_album)
-    album = Album.find_or_create_by(spotify_id: spotify_album.id) do |album|
+    artist.albums.find_or_create_by(spotify_id: spotify_album.id) do |album|
       puts "Creating #{album.name}"
       album.name = spotify_album.name
       album.image = spotify_album.images.first["url"]
@@ -43,7 +43,9 @@ class SpotifyImporter
     end
   end
   def create_song(album, spotify_track)
-    song = Song.find_or_create_by(spotify_id: spotify_track.id) do |song|
+    sleep 1 #prevent reaching  spotify api limit. 
+
+    album.songs.find_or_create_by(spotify_id: spotify_track.id) do |song|
       puts "Creating #{song.name}"
       song.name = spotify_track.name
       song.spotify_url = spotify_track.href
