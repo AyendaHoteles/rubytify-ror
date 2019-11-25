9c261a453ab3656e8c61d3dd392cd8531fbf0878
roberto rueda <rueda.roberto89@gmail.com>
2019-11-25 10:48:00 -0500

    Update spotify_importer Prevent duplicated records

--- lib/spotify_importer.rb
+++ lib/spotify_importer.rb
@@ -11,7 +11,7 @@ class SpotifyImporter
         spotify_artist.albums.each do |spotify_album|
           album = create_album(artist, spotify_album)
           spotify_album.tracks.each do |spotify_track|
-            create_song(album, spotify_track)
+            song = create_song(album, spotify_track)
           end
         end
       else
@@ -20,32 +20,38 @@ class SpotifyImporter
     end
   end
   def create_artist(spotify_artist)
-    artist = Artist.create(
-      name: spotify_artist.name,
-      image: spotify_artist.images.first["url"],
-      genres: spotify_artist.genres,
-      popularity: spotify_artist.popularity,
-      spotify_url: spotify_artist.href,
-      spotify_id: spotify_artist.id
-      )
+    artist = Artist.find_or_create_by(spotify_id: spotify_artist.id) do |artist|
+      puts "Creating #{artist.name} in the DB"
+      artist.name = spotify_artist.name
+      artist.image = spotify_artist.images.first["url"]
+      artist.genres = spotify_artist.genres
+      artist.popularity = spotify_artist.popularity
+      artist.spotify_url = spotify_artist.href
+      artist.spotify_id = spotify_artist.id
+      puts "the artist #{artist.name} has been created"
+    end
   end
   def create_album(artist, spotify_album)
-    artist.albums.create(
-      name: spotify_album.name,
-      image: spotify_album.images.first["url"],
-      spotify_url: spotify_album.href,
-      total_tracks: spotify_album.total_tracks,
-      spotify_id: spotify_album.id
-    )
+    album = Album.find_or_create_by(spotify_id: spotify_album.id) do |album|
+      puts "Creating #{album.name}"
+      album.name = spotify_album.name
+      album.image = spotify_album.images.first["url"]
+      album.spotify_url = spotify_album.href
+      album.total_tracks = spotify_album.total_tracks
+      album.spotify_id = spotify_album.id
+      puts "the album #{album.name} has been created"
+    end
   end
   def create_song(album, spotify_track)
-    album.songs.create(
-      name: spotify_track.name,
-      spotify_url: spotify_track.href,
-      preview_url: spotify_track.preview_url,
-      duration_ms: spotify_track.duration_ms,
-      explicit: spotify_track.explicit,
-      spotify_id: spotify_track.id
-    )
+    song = Song.find_or_create_by(spotify_id: spotify_track.id) do |song|
+      puts "Creating #{song.name}"
+      song.name = spotify_track.name
+      song.spotify_url = spotify_track.href
+      song.preview_url = spotify_track.preview_url
+      song.duration_ms = spotify_track.duration_ms
+      song.explicit = spotify_track.explicit
+      song.spotify_id = spotify_track.id
+      puts "the song #{song.name} has been created"
+    end
   end
 end
