ff8fd399be8958a1d0be4d395ff9f47930976cac
Mauricio Vasquez <usuario@MacBook-Air-de-Usuario.local>
2019-11-26 14:49:49 -0500

    models, migrations, model_sped, seed and factories

---
+++ db/migrate/20191123013925_create_artists.rb
@@ -0,0 +1,15 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artists do |t|
+      t.string :name, nill: false
+      t.string :image
+      t.string :genres, array: true
+      t.integer :popularity
+      t.string :spotify_url
+      t.string :spotify_id
+
+      t.timestamps
+    end
+    add_index :artists, :genres, using: 'gin'
+  end
+end
