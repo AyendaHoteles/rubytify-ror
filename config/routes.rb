9abb4e18d3ece4fe7f7027fbc95dd5166a4368a5
Jorge Orlando Muñoz <jmunoz@altavistaed.com>
2019-12-13 17:04:25 +0000

    config: Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'api/v1/artists#index'
+  namespace :api do
+    namespace :v1 do
+      resources :albums, only: [] do
+        resources :songs, only: [:index], defaults: { format: :json }
+      end
+      resources :artists, only: [:index], defaults: { format: :json } do
+        get '/albums', defaults: { format: :json }, to: 'artists#albums'
+      end
+      get 'genres/:genre_name/random_song', defaults: { format: :json },
+                                            to: 'songs#random_song',
+                                            as: :genre_random_song
+    end
+  end
 end
