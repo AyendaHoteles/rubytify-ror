7605cec3a89592d057a550fbdad825461a819720
Rafael Carrascal Reyes <rcarrascal@gmail.com>
2019-12-30 10:52:27 -0500

    models and db

---
+++ db/migrate/20191228165603_create_albums.rb
@@ -0,0 +1,14 @@
+class CreateAlbums < ActiveRecord::Migration[5.2]
+  def change
+    create_table :albums do |t|
+      t.string :name
+      t.string :image
+      t.string :spotify_url
+      t.string :total_tracks
+      t.string :spotify_id
+      t.integer :artist_id
+
+      t.timestamps
+    end
+  end
+end
