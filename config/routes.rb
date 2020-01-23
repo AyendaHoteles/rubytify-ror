c1e30540649f53cc81c29c50d8f9ed5039a6c397
amartinezre05 <almartinezre05@gmail.com>
2020-01-23 01:25:19 -0500

    [add] root for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root :to => "api/v1/artists#index"
   namespace "api" do
     namespace "v1" do
       resources :artists, only: [:index] 
