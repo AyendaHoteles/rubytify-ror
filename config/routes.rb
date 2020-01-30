15ad1ac1331f3d7d969c64469bcfe58afd6b89e2
Miguel Ferrer <miguef7@gmail.com>
2020-01-30 15:32:20 -0500

    add route for random song endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
           resources :songs, only: :index
         end
       end
+      get '/genres/:genre_name/random_song', to: 'songs#random', as: 'random_song'
       match '*unmatched', to: 'base#bad_request', via: :all
     end
   end
