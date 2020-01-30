6668478d21888b0ec16ca40b29bf38c1fb3760bf
Miguel Ferrer <miguef7@gmail.com>
2020-01-30 11:36:35 -0500

    add controller and routess for index action on albums, and code that renders the required format

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  require "sidekiq/web"
-  mount Sidekiq::Web => '/sidekiq'
+  require 'sidekiq/web'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :artists, only: :index
+      resources :artists, only: :index do
+        resources :albums, only: :index
+      end
       match '*unmatched', to: 'base#bad_request', via: :all
     end
   end
+  mount Sidekiq::Web => '/sidekiq'
 end
