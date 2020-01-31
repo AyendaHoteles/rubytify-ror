1c50f4e8dfb0fd3cb40036f5fc19dd69039be87b
bigfaiber <greatfaiber@gmail.com>
2020-01-31 00:13:52 -0500

    basic validations for artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,7 @@
 class Artist < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :spotify_id
+  validates_presence_of :spotify_url
+
+  has_many :albums
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should validate required fields" do
+      should validate_presence_of(:name)
+      should validate_presence_of(:spotify_url)
+      should validate_presence_of(:spotify_id)
+    end
+
+    it "should has many albums" do
+      should have_many(:albums)
+      should have_many(:songs)
+    end
+  end
 end
