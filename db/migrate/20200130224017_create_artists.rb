fcb37f382626fb80f915e48e93722d7f2d4dd680
bigfaiber <greatfaiber@gmail.com>
2020-01-30 23:25:29 -0500

    artist model created

--- db/migrate/20200130224017_create_artists.rb
+++ db/migrate/20200130224017_create_artists.rb
@@ -3,10 +3,11 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     create_table :artists do |t|
       t.string :name
       t.string :image
-      t.array :genres
+      t.text :genres, array: true
       t.integer :popularity
       t.string :spotify_url
       t.string :spotify_id
+      t.timestamps
     end
   end
 end
