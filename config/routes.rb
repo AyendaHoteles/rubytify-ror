1526f1b4e6cc54fadae715a694409fb99c84a43b
bigfaiber <greatfaiber@gmail.com>
2020-02-03 00:25:00 -0500

    routes for all endpoints required

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :artists do
-        resources :albums
-        resources :songs
+      resources :artists, only: :index do
+        resources :albums, only: :index
       end
+      resources :albums, only: :show do
+        resources :songs, only: :index
+      end
+      get 'genres/:genre_name/random_song', to: 'genres#random_song'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
