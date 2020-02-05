76d935557b45ab672093299877345e9778a2c58e
CamiMeneses <CamiMeneses@github.com>
2020-02-04 19:35:01 -0500

    Spotify migration and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+  get 'api/v1/artists', to: 'artists#index'
+  get 'api/v1/artists/:id/albums', to: 'artists#albums'
+  get 'api/v1/albums/:id/songs', to: 'albums#songs'
+  get 'api/v1/genres/:genre_name/random_song', to: 'genres#random_song'
 end
