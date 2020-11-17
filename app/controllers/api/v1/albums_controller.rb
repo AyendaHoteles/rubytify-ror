56f992574d7be49bf57b8818808f5a2fe48ab9f6
juanfer2 <jfvilladiego3@gmail.com>
2020-11-16 19:21:40 -0500

    deploy

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,12 +1,6 @@
 module Api
   module V1
       class AlbumsController < ApplicationController
-        def index
-          albums_service = AlbumsService.new
-          @albums = albums_service.get_albums_by_artist('1dfeR4HaWDbWqFHLkxsg1d')
-          render json: @albums
-        end
-
         def songs_by_album
           album = Album.find_by(id: params[:id])
           if album.present?
