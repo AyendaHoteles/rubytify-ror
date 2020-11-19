a5b8d5ffaec78e25b91ade8ccc1a2cff28628365
juanfer2 <jfvilladiego3@gmail.com>
2020-11-18 22:25:48 -0500

    correction name routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       get '/artists/:id/albums', to: 'artists#albums_by_artist', as: :albums_by_artist
       get '/albums', to: 'albums#index', as: :albums
       get '/albums/:id/songs', to: 'albums#songs_by_album', as: :songs_by_album
-      get '/genres/:genre_name/songs', to: 'genres#aleatory_song_by_gender', as: :songs_by_genre
+      get '/genres/:genre_name/random_song', to: 'genres#aleatory_song_by_gender', as: :songs_by_genre
     end
   end
 end
