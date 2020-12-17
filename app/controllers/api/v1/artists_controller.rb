44152496fbbf2ef40a0245026856fcedea507dd8
Julian P <julian.pineda21@gmail.com>
2020-12-17 09:11:41 -0500

    fix return all artists ordered by their popularity

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    @artists = Artist.all.order(popularity: :desc)
     render json: @artists, root: 'data', adapter: :json
   end
 end
