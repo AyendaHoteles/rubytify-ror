f71cc0eaec23dbff869af43d57cfa866350a66d6
Bryan <nuevopa@gmail.com>
2021-05-29 18:51:11 -0500

    Creacion de endpoint "/api/v1/genres/:genre_name/random_song"

---
+++ app/controllers/api/v1/genres_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::GenresController < ApplicationController
+
+   #GET /api/v1/genres/:genre_name/random_song
+   def random_song
+   
+      search = "genres LIKE '%" + params[:genre_name] + "%'"
+      @artists = Artist.all.where(search)
+      
+      len_artists = @artists.length
+      pos_artist = rand(len_artists)
+      @albums = Album.where("artist_id = ?", @artists[pos_artist].id)
+      
+      len_albums = @albums.length
+      pos_album = rand(len_albums)
+      @songs = Song.where("album_id = ?", @albums[pos_album].id)
+      
+      len_song = @songs.length
+      pos_song = rand(len_song)
+      
+      render json: @songs[pos_song], root: 'data', each_serializer: SongSerializer, adapter: :json
+   end
+end
