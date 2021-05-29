0027402ed705bd8c6bb499675c7c537b3797e820
Bryan <nuevopa@gmail.com>
2021-05-29 18:51:11 -0500

    Creacion de endpoint "/api/v1/genres/:genre_name/random_song"

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
          resources :artists, only: [:index] do
             resources :albums, only: [:index]
          end
+         
          resources :albums, only: [] do
             resources :songs, only: [:index]
          end
+         
+         get '/genres/:genre_name/random_song', to: 'genres#random_song'
       end
    end
 end
