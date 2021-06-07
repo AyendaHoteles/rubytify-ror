962c6d7eaa82561dafa1884e7c391c2a4c7a2669
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-06-07 14:01:30 -0500

    Adjust again the json format

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::AlbumsController < ApplicationController
   #GET /api/v1/artists/:id/albums
   def index
+    @artist = Artist.find_by(params[:id])
+    #@albums = Album.where(artist_id: @artist.id)
     @albums = Album.where("artist_id = ?", params[:artist_id])
-    render json: { data: @albums }, each_serializer: AlbumSerializer, status: :ok
+    render json: @albums, root: 'data', each_serializer: AlbumSerializer, adapter: :json, status: :ok
   end
 end
--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ArtistsController < ApplicationController
   #GET /api/v1/artists
   def index
     @artists = Artist.all.order("popularity DESC")
-    render json: { data: @artists }, each_serializer: ArtistSerializer, status: :ok
+    render json: @artists, root: 'data', each_serializer: ArtistSerializer, adapter: :json, status: :ok
   end
 end
