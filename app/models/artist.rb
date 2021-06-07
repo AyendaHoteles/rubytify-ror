5051c17fb0869a3a3e4177c93c27a2c4451b4caf
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-06-07 15:14:22 -0500

    Delete validation

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,6 @@ class Artist < ApplicationRecord
   has_many :songs, through: :albums
 
   validates :name, presence: true
-  validates :image, presence: true
   validates :genres, presence: true
   validates :popularity, presence: true
   validates :spotify_url, presence: true
