02e19a1309297bb8eab1b879ca487eee10b7dac0
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-06-07 15:15:31 -0500

    Modify code that get the albums list

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::AlbumsController < ApplicationController
   #GET /api/v1/artists/:id/albums
   def index
-    @artist = Artist.find_by(params[:id])
-    #@albums = Album.where(artist_id: @artist.id)
-    @albums = Album.where("artist_id = ?", params[:artist_id])
+    @albums = Artist.find(params[:artist_id]).albums
     render json: @albums, root: 'data', each_serializer: AlbumSerializer, adapter: :json, status: :ok
   end
 end
