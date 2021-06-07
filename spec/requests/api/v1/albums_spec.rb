80ff4a550ddf4f367a65e8a1bc9ac6623be048db
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-06-07 15:13:01 -0500

    Fix tests to Albums and Artists endpoints

--- spec/requests/api/v1/albums_spec.rb
+++ spec/requests/api/v1/albums_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Albums', type: :request do
+  before(:each) do
+    @artist = create :artist
+    @albums = create_list(:album, 10, artist_id: @artist.id)
+  end
+
   describe 'GET /api/v1/artists/:id/albums' do
     it 'should return all albums from an artist' do
-      get '/api/v1/artists/artist_id/albums'
+      get "/api/v1/artists/#{@artist.id}/albums"
       payload = JSON.parse(response.body)
-      #expect(payload.size).to eq(2)
+      expect(payload['data'].size).to eq(10)
       expect(response).to have_http_status(200)
     end
   end
