9bff8db2764483429ab55febb9effe003354e76b
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-06-07 13:12:41 -0500

    Modify code to json format

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::ArtistsController < ApplicationController
-
   #GET /api/v1/artists
   def index
     @artists = Artist.all.order("popularity DESC")
-    render json: @artists, root: 'data', each_serializer: ArtistSerializer, adapter: :json, status: :ok
+    render json: { data: @artists }, each_serializer: ArtistSerializer, status: :ok
   end
-
 end
