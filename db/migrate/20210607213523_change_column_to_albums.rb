cda6bc4046e074e233cc4330b070dfb6defe44c1
Nathaly Sotomayor <nathysoto12.07@gmail.com>
2021-06-07 21:19:00 -0500

    Add sentence to migration files to handle postgresql json data type

--- db/migrate/20210607213523_change_column_to_albums.rb
+++ db/migrate/20210607213523_change_column_to_albums.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToAlbums < ActiveRecord::Migration[5.2]
   def change
-    change_column(:albums, :image, :json)
+    change_column(:albums, :image, :json, using: 'image::JSON')
   end
 end
--- db/migrate/20210607213653_change_column_to_artists.rb
+++ db/migrate/20210607213653_change_column_to_artists.rb
@@ -1,6 +1,6 @@
 class ChangeColumnToArtists < ActiveRecord::Migration[5.2]
   def change
-    change_column(:artists, :image, :json)
-    change_column(:artists, :genres, :json)
+    change_column(:artists, :image, :json, using: 'image::JSON')
+    change_column(:artists, :genres, :json, using: 'genres::JSON')
   end
 end
